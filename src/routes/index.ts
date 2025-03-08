6743472c319e690c6b307b4ec51f450f1b7996f3
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2025-03-08 21:03:49 +0530

    added vehicle owner registration endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import vehicleOwnerRoutes from "../controllers/vehicleowner.controller";
 import auditLogRoutes from "../controllers/auditlog.controller"
+import authRoutes from "../controllers/auth.controller"
 
 const router = express.Router();
 
 router.use("/vehicle-owner", vehicleOwnerRoutes);
 router.use("/audit-log" , auditLogRoutes)
+router.use("/auth" , authRoutes)
 
 export default router;
