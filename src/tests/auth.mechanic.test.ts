12d8c3b9e495775e2fa4159a3732141a306fa13f
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2025-03-16 14:29:43 +0530

    fixed test issues

--- src/tests/auth.mechanic.test.ts
+++ src/tests/auth.mechanic.test.ts
@@ -16,7 +16,7 @@ describe("Mechanic Authentication Routes", () => {
         jest.setTimeout(30000);
 
         process.env.DATABASE_URL = "file:./test.db?mode=memory";
-        await execSync('npx prisma db push');
+        await execSync('npx prisma migrate deploy');
 
         await prisma.authToken.deleteMany();
         await prisma.mechanic.deleteMany();
