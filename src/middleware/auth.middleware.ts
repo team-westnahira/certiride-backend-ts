b97f12b5e41b16bf2cd15aa96ee688f0ab8049c3
Hiroshan375 <hettiarachchihiroshan01@gmail.com>
2025-03-17 10:38:42 +0530

    aadded changes

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -20,7 +20,7 @@ const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => 
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: string; email: string };
-        req.user = decoded;
+        req.user = decod
         next();
     } catch (err) {
         return res.status(403).json({ error: "Invalid or expired token" });
