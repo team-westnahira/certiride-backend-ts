027604a28e37cf6b9b1ebc22480fad0865916942
Haritha Senadheera <harithasenadheera1@gmail.com>
2025-03-27 12:47:20 +0530

    controller patched

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ import auditLogRoutes from "../controllers/auditlog.controller";
 import adminAuth from "../controllers/auth/admin-auth.controller";
 import mechanicAuth from "../controllers/auth/mechanic-auth.controller";
 import vehicleOwnerAuth from "../controllers/auth/vehicle-owner-auth.controller";
-import vehicleController from "../controllers/vehicleregisterendpoint.controller";
+import vehicleInfo from "../controllers/vehicledetails.controller";
 const router = express.Router();
 
 router.use("/vehicle-owner", vehicleOwnerRoutes);
@@ -15,6 +15,6 @@ router.use("/audit-log", auditLogRoutes);
 router.use("/auth/admin", adminAuth);
 router.use("/auth/mechanic", mechanicAuth);
 router.use("/auth/vehicle-owner", vehicleOwnerAuth);
-router.use("/register-vehicle", vehicleController);
+router.use("/vehicle-info" , vehicleInfo)
 
 export default router;
