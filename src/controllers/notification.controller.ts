a881cef12f7424b4f0c23865b7d77dee5e43c72b
Naveendanj <naveenhettiwaththa@gmail.com>
2025-07-14 22:23:05 +0530

    added endpoint for notifications

---
+++ src/controllers/notification.controller.ts
@@ -0,0 +1,51 @@
+import { Response } from "express";
+import prisma from "../config/prisma";
+import { AuthenticatedVehicleOwnerRequest } from "../types";
+
+export const getNotifications = async (req: AuthenticatedVehicleOwnerRequest, res: Response) => {
+  const { userRole , all } = req.query as { userRole: string , all?: string };
+
+  try {
+    const notifications = await prisma.notification.findMany({
+      where: {
+        userId: req.user?.id,
+        userRole,
+        read: all !== 'true' ? false : undefined,
+      },
+      orderBy: {
+        date: "desc",
+      },
+    });
+
+    res.json({ success: true, data: notifications });
+    return
+  } catch (error) {
+    console.error("Error fetching notifications", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+    return;
+  }
+};
+
+export const markAllNotificationsAsRead = async (req: AuthenticatedVehicleOwnerRequest, res: Response) => {
+  const { userRole } = req.query as { userRole: string };
+
+  try {
+    await prisma.notification.updateMany({
+      where: {
+        userId: req.user?.id,
+        userRole,
+        read: false,
+      },
+      data: {
+        read: true,
+      },
+    });
+
+    res.json({ success: true, message: "All notifications marked as read" });
+    return;
+  } catch (error) {
+    console.error("Error updating all notifications", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+    return;
+  }
+};
